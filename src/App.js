e732bf9965ef1c6466428416df50a99d658b70be
Serhii-P <serhii.pyskun@gmail.com>
2022-06-09 12:29:34 +0300

    route testing3

--- src/App.js
+++ src/App.js
@@ -13,8 +13,16 @@ const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/rickandmorty-test/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Character page={page}  setPage={setPage}/>} />
+          <Route path="/:characterId" element={<CharacterDetails />} />
+          <Route path="search" element={<Search />} />
+          <Route path="/search/character/:characterId" element={<CharacterDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+
+        <Route path="/rickandmorty-test/" element={<Layout />}>
+          <Route path="/rickandmorty-test/" element={<Character page={page}  setPage={setPage}/>} />
           <Route path="/:characterId" element={<CharacterDetails />} />
           <Route path="search" element={<Search />} />
           <Route path="/search/character/:characterId" element={<CharacterDetails />} />
